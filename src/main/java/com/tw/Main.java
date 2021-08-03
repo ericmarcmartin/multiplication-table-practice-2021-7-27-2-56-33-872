62a132092cd2f7118a6cfb18ab74bfe0ab3a543f
martier <martier@oocl.com>
2021-08-03 21:53:14 +0800

    [Multiplication Table][Eric]feat: added Main.java and ContextMapActivity.png

---
+++ src/main/java/com/tw/Main.java
@@ -0,0 +1,8 @@
+package com.tw;
+
+public class Main {
+    public static void main(String[] args) {
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        multiplicationTable.create(2,4);
+    }
+}
